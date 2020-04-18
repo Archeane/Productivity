42579c0385bb5923aefb390fbeb2edeb688be311
Archeane <jennyxu8448@gmail.com>
2020-04-18 13:24:01 -0400

    wtf

--- src/options/options.js
+++ src/options/options.js
@@ -2,14 +2,10 @@ import Vue from 'vue';
 import App from './App';
 import vuetify from '../plugins/vuetify';
 import Vuetify from 'vuetify';
-import Dashboard from './components/Dashboard.vue'
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: {
-    "Dashboard": Dashboard
-  },
   vuetify: new Vuetify({
     theme: {
       dark: true,
